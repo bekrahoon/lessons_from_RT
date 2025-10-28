730ddb0d01f93fb443dd99977f1d2a4430f7e72a
Bekrahun <bekrahoon12@gmail.com>
2025-10-28 11:23:20 +0600

    изменено:      js/lesson.js изменено:      pages/lessons.html

--- js/lesson.js
+++ js/lesson.js
@@ -1,20 +1,85 @@
-// Проверка номера
+//проверка номера
 const phoneInput = document.querySelector('#phone_input');
 const phoneButton = document.querySelector('#phone_button');
 const phoneSpan = document.querySelector('#phone_result');
 
-// Регулярки для Кыргызстана и России
-const kgRegExp = /^\+996[2579]\d{7}$/;                  // пример: +996550644772
-const ruRegExp = /^\+7\s?\d{3}\s?\d{3}-\d{2}-\d{2}$/;   // пример: +7 999 123-45-67
 
-phoneButton.addEventListener('click', () => {
-    const value = phoneInput.value.trim();
+//+996550644772
+const reqExp = /^\+996 [2579]\d{2} \d{2}-\d{2}-\d{2}$/
 
-    if (kgRegExp.test(value) || ruRegExp.test(value)) {
+phoneButton.addEventListener('click', ()=>{
+    if (reqExp.test(phoneInput.value)){
         phoneSpan.innerHTML = 'Этот номер существует';
         phoneSpan.style.color = 'green';
-    } else {
+    }else {
         phoneSpan.innerHTML = 'Этот номер не существует';
         phoneSpan.style.color = 'red';
     }
-});
+})
+
+
+//TAB SLIDER
+const tabsContentCards = document.querySelectorAll('.tab_content_block');
+const tabsItems = document.querySelectorAll('.tab_content_item');
+const tabsItemsParents =  document.querySelector('.tab_content_items');
+
+
+const hightTabsContentCards = () =>{
+    tabsContentCards.forEach((tabsContentCard)=>{
+        tabsContentCard.style.display = 'none'
+    })
+    tabsItems.forEach((tabItem)=>{
+        tabItem.classList.remove('tab_content_item_active')
+    })
+}
+
+const showTabsContentCards = (indexElement = 0)=>{
+    tabsContentCards[indexElement].style.display = 'block';
+    tabsItems[indexElement].classList.add('tab_content_item_active')
+}
+
+hightTabsContentCards();
+showTabsContentCards();
+
+
+tabsItemsParents.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')){
+        tabsItems.forEach((tabItem, tabItemIndex)=>{
+            if(event.target === tabItem){
+                hightTabsContentCards()
+                showTabsContentCards(tabItemIndex)
+            }
+        })
+    }
+}
+
+let curretIndex = 0; // Первая вкладка
+let intervalId; //Переменная для хранения интервала
+
+//Ф-ция для автоматического переключения
+
+const startAuthoSlider = ()=>{
+    intervalId = setInterval(()=>{
+        hightTabsContentCards();
+        showTabsContentCards(curretIndex);
+        curretIndex = (curretIndex +1) % tabsItems.length;
+    }, 2000); // 2сек
+}
+//Запуск автослайдера
+startAuthoSlider();
+
+//Остановка слайдера при клике на вкладку
+
+tabsItemsParents.onclick = (event) => {
+    clearInterval(intervalId);
+    if (event.target.classList.contains('tab_content_item')){
+        tabsItems.forEach((tabItem, tabItemIndex) =>{
+            if(event.target === tabItem){
+                hightTabsContentCards();
+                showTabsContentCards(tabItemIndex);
+                curretIndex = tabItemIndex;
+                startAuthoSlider();
+            }
+        })
+    }
+}
