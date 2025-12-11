eba8b360681a05d28174e7d83f6b89e1ed8f0453
Bekrahun <bekrahoon12@gmail.com>
2025-12-11 14:08:22 +0600

    Add movie detail and movie list components

--- my-website/src/MovieDetail.js
+++ my-website/src/MovieDetail.js
@@ -1,3 +1,4 @@
+// my-website/src/MovieDetail.js
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
 
--- my-website/src/MovieList.js
+++ my-website/src/MovieList.js
@@ -1,3 +1,4 @@
+// my-website/src/MovieList.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 
